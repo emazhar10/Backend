85587fe20c8f01365f3eb4c0b776bebcc0a1fa78
emazhar10 <eesha.mazhar@devxperts.co>
2022-08-03 14:30:33 +0500

    post finalize

--- index.js
+++ index.js
@@ -18,39 +18,23 @@ async function asyncMain() {
     const eesha = {
       firstName,
       lastName,
-      email: "eesha36@gmail.com",
+      email: "eesha48@gmail.com",
       password: "12345678",
       role: "user",
     };
     const user1 = await User.create(eesha);
-    const post  = await Posts.build({
-      body: "xyz",
-      title: "information ",
-    })
-    await user1.addPost(post)
-    await post.save()
-    await user1.save()
+    const post  = {
+      body: "xyz\n\n\n\n",
+      title: "information1 ",
+      userId: user1.id
+    }
+    const post1 = await Posts.create(post);
+   
+    await post1.save()
     console.log(user1);
   } catch (error) {
     console.error("Unable to insert to the database:", error);
   }
 
-  // try {
-  //   const post_data = {
-  //     body: "xyz",
-  //     title: "information ",
-  //     userId: 1,
-  //   };
-  //   console.log("workingggg");
-  //   const data = await Posts.create(post_data);
-  //   console.log(data.userId, "datacoming");
-  // } catch (error) {
-  //   console.log("Unable to enter post data in database", error);
-  // }
-
-  // const users = await User.findAll({
-  //   order: ["id"],
-  // });
-  // console.log(users[0]);
 }
 asyncMain();
--- models/post.js
+++ models/post.js
@@ -8,10 +8,35 @@ module.exports = (sequelize, DataTypes) => {
   }
   Posts.init(
     {
-      body: DataTypes.STRING,
-      title: DataTypes.STRING,
+      body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isValid: (value) => {
+            var count = (value.match(/\n/g) || []).length;
+            if (count < 4) {
+              throw new Error("count of paragraph should not be less then 5");
+            }
+          },
+        },
+      },
       userId: DataTypes.INTEGER,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isUnique: async (value) => {
+            const post = await Posts.findOne({ where: { title: value } });
+            console.log(post);
+            if (post) {
+              throw new Error("post with this title already exists");
+            }
+          },
+        },
+      },
     },
+
     {
       sequelize,
       modelName: "Posts",
